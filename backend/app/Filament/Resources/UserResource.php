54880d6f954c4c920b619c8991597accc7b56264
Daniyar <dancho.rancho07@gmail.com>
2026-01-28 18:46:12 +0100

    Fix UserResource navigation icon typing

--- backend/app/Filament/Resources/UserResource.php
+++ backend/app/Filament/Resources/UserResource.php
@@ -12,13 +12,14 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use BackedEnum;
 use UnitEnum;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-users';
 
     protected static string|UnitEnum|null $navigationGroup = 'Role Panel';
 
