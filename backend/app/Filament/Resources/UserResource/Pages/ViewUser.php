884db0f115c44d9dd3793e610f9e60b464abbe8f
Daniyar Amirov <dancho.rancho07@gmail.com>
2026-01-29 21:30:20 +0100

    Fix: Edit and Delete actions for View user page added

--- backend/app/Filament/Resources/UserResource/Pages/ViewUser.php
+++ backend/app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource\UserResource;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Actions;
 
 class ViewUser extends ViewRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
