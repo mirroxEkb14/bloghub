50ec85851dc100def2ccb43dc7d2f341dafe912b
Daniyar <dancho.rancho07@gmail.com>
2026-01-29 15:47:18 +0100

    Adjust password validation translations

--- backend/resources/lang/cs/validation.php
+++ backend/resources/lang/cs/validation.php
@@ -3,16 +3,13 @@
 return [
     'email' => 'Pole :attribute musí být platná e-mailová adresa',
     'phone' => 'Formát pole :attribute je neplatný',
-    'min' => [
-        'string' => 'Pole :attribute musí mít alespoň :min znaků.',
-    ],
     'password' => [
-        'letters' => 'Pole :attribute musí obsahovat alespoň jedno písmeno.',
-        'mixed' => 'Pole :attribute musí obsahovat alespoň jedno velké a jedno malé písmeno.',
-        'numbers' => 'Pole :attribute musí obsahovat alespoň jednu číslici.',
-        'symbols' => 'Pole :attribute musí obsahovat alespoň jeden speciální znak.',
-        'uncompromised' => 'Pole :attribute se objevilo v úniku dat. Zvolte prosím jiné :attribute.',
-        'min' => 'Pole :attribute musí mít alespoň :min znaků.',
+        'letters' => 'Pole :attribute musí obsahovat alespoň jedno písmeno',
+        'mixed' => 'Pole :attribute musí obsahovat alespoň jedno velké a jedno malé písmeno',
+        'numbers' => 'Pole :attribute musí obsahovat alespoň jednu číslici',
+        'symbols' => 'Pole :attribute musí obsahovat alespoň jeden speciální znak',
+        'uncompromised' => 'Pole :attribute se objevilo v úniku dat. Zvolte prosím jiné :attribute',
+        'min' => 'Pole :attribute musí mít alespoň :min znaků',
     ],
-    'password_contains_user_data' => 'Pole :attribute nesmí obsahovat hodnotu :field.',
+    'password_contains_user_data' => 'Pole :attribute nesmí obsahovat hodnotu :field',
 ];
