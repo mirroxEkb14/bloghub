c1c99962b9ff7f62eb75e348761acbddb7f13b47
Daniyar Amirov <dancho.rancho07@gmail.com>
2026-01-30 17:21:49 +0100

    Fix: migration for Users added, ER model updated

---
+++ backend/database/migrations/2026_01_30_141129_adjust_users_columns_constraints.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', 100)->change();
+            $table->string('username', 50)->change();
+            $table->string('phone', 16)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', 255)->change();
+            $table->string('username', 255)->change();
+            $table->string('phone', 255)->nullable()->change();
+        });
+    }
+};
