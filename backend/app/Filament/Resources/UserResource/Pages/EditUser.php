14dc22c846a36d4011f261ad9dde1221b02327b4
Daniyar Amirov <dancho.rancho07@gmail.com>
2026-01-30 04:53:58 +0100

    Fix: DeleteAction for Edit user page added

--- backend/app/Filament/Resources/UserResource/Pages/EditUser.php
+++ backend/app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -4,8 +4,16 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource\UserResource;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Actions;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
