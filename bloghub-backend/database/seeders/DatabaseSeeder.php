d4dd1be8b5cebb2980bb3615dcb69ccb7e17af1c
Daniyar <dancho.rancho07@gmail.com>
2026-03-12 19:47:11 +0100

    Seeders: follow seeder added

--- bloghub-backend/database/seeders/DatabaseSeeder.php
+++ bloghub-backend/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             UsersSeeder::class,
             TagSeeder::class,
             CreatorProfileSeeder::class,
+            CreatorProfileFollowSeeder::class,
             TierSeeder::class,
             PostSeeder::class,
             SubscriptionSeeder::class,
